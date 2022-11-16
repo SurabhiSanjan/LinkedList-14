e7608da9c227c0e886a3b4d03815327efc1041fd
SurabhiSanjan <surabhisanjan@gmail.com>
2022-11-16 19:23:08 +0530

    Modification: Adding values in the LinkedList

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -1,10 +1,50 @@
 package com.bridgelabz.linkedlist;
 
 public class LinkedList {
+    Node head;
+    class Node {
+        int data;
+        Node next;
 
-    public static void main(String[] args) {
-
-        System.out.println("Welcome to LinkedList Practice Problem");
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
     }
-}
+    public void printNode () {
+        if (head == null) {
+            System.out.println("Linked list is empty...");
+            return;
+        }
+        Node tempNode = head;
+        while (tempNode != null) {
+            System.out.print(tempNode.data + "=>");
+            tempNode = tempNode.next;
+        }
+        System.out.println("null  is our current linked list");
+    }
+    public void addLast(int data) {
+        Node newNode = new Node(data);
+        if (head == null) {
+            head = newNode;
+            return;
+        }
+        Node tempNode = head;
+        while(tempNode.next != null){
+            tempNode= tempNode.next;
+        }
+        tempNode.next = newNode;
+    }
+    
+
+        public static void main (String[] args){
+            System.out.println("Welcome to LinkedList Practice Problem");
+            LinkedList ll = new LinkedList();
+            ll.addLast(56);
+            ll.addLast(30);
+            ll.addLast(70);
+            ll.printNode();
+        }
+    }
+
 
