d3da8a5fcadeb45706821c481301c8475e1f2997
SurabhiSanjan <surabhisanjan@gmail.com>
2022-11-16 19:06:12 +0530

    Initializing a new LinkedList welcome message

--- src/com/bridgelabz/linkedlist/LinkedList.java
+++ src/com/bridgelabz/linkedlist/LinkedList.java
@@ -1,50 +1,10 @@
 package com.bridgelabz.linkedlist;
 
 public class LinkedList {
-    Node head;
-    class Node {
-        int data;
-        Node next;
-
-        Node(int data){
-            this.data = data;
-            this.next = null;
-        }
-    }
-
-    public void printNode(){
-        if(head==null){
-            System.out.println("Linked list is empty...");
-            return;
-        }
-        Node tempNode = head;
-        while(tempNode != null){
-            System.out.print(tempNode.data + "=>");
-            tempNode = tempNode.next;
-        }
-        System.out.println("null");
-    }
-    public void addLast(int data){
-        Node newNode = new Node(data);
-        if(head == null){
-            head = newNode;
-            return;
-        }
-        Node tempNode = head;
-        while(tempNode.next != null){
-            tempNode= tempNode.next;
-        }
-        tempNode.next = newNode;
-    }
-
 
     public static void main(String[] args) {
-        LinkedList ll = new LinkedList();
-        ll.addLast(56);
-        ll.addLast(30);
-        ll.addLast(70);
-        ll.printNode();
 
+        System.out.println("Welcome to LinkedList Practice Problem");
     }
 }
 
